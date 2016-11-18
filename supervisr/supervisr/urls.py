9db993b1a919e9c47fbcdf0964c6005c28fb851a
Jens Langhammer <jens.langhammer@beryju.org>
2016-11-18 22:18:00 +0100

    add initial account/login logic

--- supervisr/supervisr/urls.py
+++ supervisr/supervisr/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from .views import common
+from .views import account
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', common.index)
+    url(r'^$', common.index),
+    url(r'^/accounts/login/$', account.login),
 ]
--- supervisr/supervisr/views/account.py
+++ supervisr/supervisr/views/account.py
@@ -1,22 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponseRedirect
+from .views.common import index as common_index
+from .forms.account import AuthenticationForm
 
-from .forms.account import AccountAuthenticationForm
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
+def login(req):
+    if req.method == 'POST':
+        form = AuthenticationForm(req.POST)
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
+            return redirect(reverse(common_index))
     else:
         form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
+    return render(req, 'account/login.html', { 'form': form })
