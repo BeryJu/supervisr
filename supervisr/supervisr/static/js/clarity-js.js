7165f0355114a4f953d23443a177a8ee0b68f4b5
Jens Langhammer <jens.langhammer@beryju.org>
2017-02-23 23:32:45 +0100

    add sidenav to main page, error page based on login layout

--- supervisr/supervisr/static/js/clarity-js.js
+++ supervisr/supervisr/static/js/clarity-js.js
@@ -14,79 +14,86 @@ var clrWizard = function (containerId, initialPage) {
     var totalPages = $(containerId + ' .clr-nav-content').length - 1; // - 1 because we're 0 based
 
     var updateCurrent = function (page, lastPage) {
-      console.log('page: ' + page + ', lastPage: ' + lastPage);
-      // Update main container
-      $(containerId + ' div.clr-nav-content:eq('+lastPage+')').attr('data-hidden', true);
-      $(containerId + ' div.clr-nav-content:eq('+page+')').attr('data-hidden', false);
-      // Update sidebar
-      $(containerId + ' li.clr-nav-link:eq('+lastPage+')').removeClass('active');
-      $(containerId + ' li.clr-nav-link:eq('+page+')').addClass('active');
-      // Update sidebar complete class
-      if (page > lastPage) {
-        // only add complete class when we go forward
-        $(containerId + ' li.clr-nav-link:eq('+lastPage+')').addClass('complete');
-      }
-      $(containerId + ' li.clr-nav-link:eq('+page+')').removeClass('complete');
+        console.log('page: ' + page + ', lastPage: ' + lastPage);
+        // Update main container
+        $(containerId + ' div.clr-nav-content:eq('+lastPage+')').attr('data-hidden', true);
+        $(containerId + ' div.clr-nav-content:eq('+page+')').attr('data-hidden', false);
+        // Update sidebar
+        $(containerId + ' li.clr-nav-link:eq('+lastPage+')').removeClass('active');
+        $(containerId + ' li.clr-nav-link:eq('+page+')').addClass('active');
+        // Update sidebar complete class
+        if (page > lastPage) {
+          // only add complete class when we go forward
+          $(containerId + ' li.clr-nav-link:eq('+lastPage+')').addClass('complete');
+        }
+        $(containerId + ' li.clr-nav-link:eq('+page+')').removeClass('complete');
 
-      // Disable/enable buttons
+        // Disable/enable buttons
 
-      // Check if we're done
-      if (page == totalPages) {
-        $(containerId + ' [clrWizNext]').hide();
-        $(containerId + ' [clrWizFinish]').show();
-      } else {
-        $(containerId + ' [clrWizNext]').show();
-        $(containerId + ' [clrWizFinish]').hide();
-      }
+        // Check if we're done
+        if (page == totalPages) {
+            $(containerId + ' [clrWizNext]').hide();
+            $(containerId + ' [clrWizFinish]').show();
+        } else {
+            $(containerId + ' [clrWizNext]').show();
+            $(containerId + ' [clrWizFinish]').hide();
+        }
 
-      // Disable Back on start
-      if (page === 0) {
-        $(containerId + ' [clrWizBack]').addClass('disabled');
-      } else {
-        $(containerId + ' [clrWizBack]').removeClass('disabled');
-      }
+        // Disable Back on start
+        if (page === 0) {
+            $(containerId + ' [clrWizBack]').addClass('disabled');
+        } else {
+            $(containerId + ' [clrWizBack]').removeClass('disabled');
+        }
     };
 
     var reset = function () {
-      currentPage = 0;
-      // Remove all active's and complete's
-      $(containerId + ' li.clr-nav-link').removeClass('active').removeClass('complete');
+        currentPage = 0;
+        // Remove all active's and complete's
+        $(containerId + ' li.clr-nav-link').removeClass('active').removeClass('complete');
 
-      updateCurrent(0, 0);
-    }
+        updateCurrent(0, 0);
+    };
 
     // Do cancel (x and Cancel button)
     $(containerId + ' [clrWizCancel]').on('click', function (e) {
-      $(e.target).parents(containerId).hide();
+        $(e.target).parents(containerId).hide();
     });
 
     $(containerId + ' [clrWizBack]').on('click', function (e) {
-      if (currentPage === 0) { return; }
-      currentPage -= 1;
-      updateCurrent(currentPage, currentPage + 1);
+        if (currentPage === 0) { return; }
+        currentPage -= 1;
+        updateCurrent(currentPage, currentPage + 1);
     });
 
     $(containerId + ' [clrWizNext]').on('click', function (e) {
-      updateCurrent(currentPage + 1, currentPage);
-      currentPage += 1;
+        updateCurrent(currentPage + 1, currentPage);
+        currentPage += 1;
     })
 
     $(containerId + ' li.clr-nav-link button.btn').on('click', function (e) {
-      var parent = $(e.target).parents('li.clr-nav-link');
-      var listChildren = $(e.target).parents('ol.navList').children();
-      var idx = listChildren.index(parent);
-      if ((idx > currentPage + maxStepForward) || (idx < currentPage - maxStepForward)) { return; }
-      updateCurrent(idx, currentPage);
-      currentPage = idx;
+        var parent = $(e.target).parents('li.clr-nav-link');
+        var listChildren = $(e.target).parents('ol.navList').children();
+        var idx = listChildren.index(parent);
+        if ((idx > currentPage + maxStepForward) || (idx < currentPage - maxStepForward)) { return; }
+        updateCurrent(idx, currentPage);
+        currentPage = idx;
     });
 
     // Show by button click
     $('[clrWizLauncher][data-id="'+containerId+'"]').on('click', function (e) {
-      reset();
-      $(containerId).show();
+        reset();
+        $(containerId).show();
     })
 
     reset();
 
     $(containerId).hide();
+
+    return {
+        'reset': reset,
+        'launch': function () {
+            $(containerId).show();
+        }
+    };
 };
