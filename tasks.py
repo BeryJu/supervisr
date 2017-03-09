d1a0b647c7e5bb64093e8ac5bd5f36c1dc7a4a53
Jens Langhammer <jens.langhammer@beryju.org>
2017-03-09 20:57:33 +0100

    move all tasks to invoke

--- tasks.py
+++ tasks.py
@@ -6,10 +6,11 @@ try:
     from invoke import task
     from invoke.platform import WINDOWS
 except ImportError:
-     print("Could not import pyinvoke. Please install by running 'sudo pip3 install invoke'")
+    print("Could not import pyinvoke. Please install by running 'sudo pip3 install invoke'")
 
 import os
 import sys
+from functools import wraps
 from glob import glob
 
 if WINDOWS:
@@ -26,7 +27,36 @@ def _sudo(ctx, *args, **kwargs):
     else:
         return ctx.run(*args, **kwargs)
 
+def shell(func):
+    """
+    Fixes the Shell on Windows Systems
+    """
+    @wraps(func)
+    def wrapped(ctx, *args, **kwargs):
+        """
+        Fixes the Shell on Windows Systems
+        """
+        if WINDOWS:
+            ctx.config.run.shell = "C:\\Windows\\System32\\cmd.exe"
+        return func(ctx, *args, **kwargs)
+    return wrapped
+
+def hide(func):
+    """
+    Hides the STDOUT of ctx.run
+    """
+    @wraps(func)
+    def wrapped(ctx, *args, **kwargs):
+        """
+        Hides the STDOUT of ctx.run
+        """
+        ctx.config.run.hide = 'stdout'
+        ret = func(ctx, *args, **kwargs)
+        return ret
+    return wrapped
+
 @task
+# pylint: disable=unused-argument
 def clean(ctx):
     """
     Clean Python cached files
@@ -54,29 +84,82 @@ def deploy(ctx, user=None, fqdn=None):
     ctx.run("ssh -o 'StrictHostKeyChecking=no' %s@%s \"./update_supervisr.sh\"" % (user, fqdn))
 
 @task
-def dj_make_migrations(ctx):
+@shell
+def make_migrations(ctx):
     """
     Create migrations
     """
     ctx.run("%s manage.py makemigrations" % PYTHON_EXEC)
 
-@task(pre=[dj_make_migrations])
-def dj_migrate(ctx):
+@task(pre=[make_migrations])
+@shell
+@hide
+def migrate(ctx):
     """
-    Apply  migrations
+    Apply migrations
     """
     ctx.run("%s manage.py migrate" % PYTHON_EXEC)
 
-@task(pre=[dj_migrate])
-def dj_run(ctx, port=8080):
+@task(pre=[migrate])
+@shell
+def run(ctx, port=8080):
     """
     Starts a development server
     """
     ctx.run("%s manage.py runserver 0.0.0.0:%s" % (PYTHON_EXEC, port))
 
 @task
+# pylint: disable=unused-argument
 def lint(ctx):
     """
     Run PyLint
     """
-    ctx.run("pylint --load-plugins pylint_django supervisr*")
+    from pylint.lint import Run
+    args = ['--load-plugins', 'pylint_django', 'tasks.py']
+    args.extend(glob('supervisr*'))
+    Run(args)
+
+@task
+@shell
+def prospector(ctx):
+    """
+    Run prospector
+    """
+    ctx.run("prospector -I migration")
+
+@task
+@shell
+@hide
+def isort(ctx):
+    """
+    Run isort
+    """
+    ctx.run("isort -c -vb")
+
+@task(pre=[migrate])
+@shell
+@hide
+def coverage(ctx):
+    """
+    Run Unittests and get coverage
+    """
+    ctx.run("coverage run --source='.' manage.py test --pattern=Test*.py")
+    ctx.run("coverage report")
+
+@task
+@shell
+@hide
+def unittest(ctx):
+    """
+    Run Unittests
+    """
+    ctx.run("%s manage.py test --pattern=Test*.py" % PYTHON_EXEC)
+
+# Some tasks to make full testing easier
+@task(pre=[migrate, unittest, coverage, isort, lint, prospector])
+# pylint: disable=unused-argument
+def test(ctx):
+    """
+    Run all tests
+    """
+    pass
