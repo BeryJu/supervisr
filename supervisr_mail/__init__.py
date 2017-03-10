e03bec63d6cd98a64da5a02907b248d822589e92
Jens Langhammer <jens.langhammer@beryju.org>
2017-03-10 21:41:25 +0100

    clear __init__.py since we use AppConfig in settings.py

--- supervisr_mail/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-supervisr_mail.__init__
-"""
-
-# pylint: disable=invalid-name
-default_app_config = 'supervisr_mail.apps.SupervisrMailConfig'
--- supervisr_server/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-supervisr_server.__init__
-"""
-
-# pylint: disable=invalid-name
-default_app_config = 'supervisr_server.apps.SupervisrServerConfig'
