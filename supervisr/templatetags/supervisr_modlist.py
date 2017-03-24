9aec56743211badeb1c11f5a18c36d3babac59e6
Jens Langhammer <jens.langhammer@beryju.org>
2017-03-24 22:02:20 +0100

    add wizard to add mail domain
    fix mail account creation
    dynamically load apps for navbar (closes #30)
    add mailforwarder model

--- supervisr/templatetags/supervisr_modlist.py
+++ supervisr/templatetags/supervisr_modlist.py
@@ -8,24 +8,26 @@ from django.apps import apps
 from ..utils import get_apps
 
 register = template.Library()
+VIEW_LIST = []
 
 @register.simple_tag
 def supervisr_modlist():
     """
-
-    Get a setting from the database. Returns default is setting doesn't exist.
+    Get a list of all modules and their admin page
     """
+    # pylint: disable=global-statement
+    global VIEW_LIST
     mod_list = get_apps(mod_only=True)
-    view_list = []
-    for mod in mod_list:
-        if '.' in mod:
-            name = mod.split('.')[-1]
-            mod = mod.split('.')[0]
-        else:
-            name = mod
-        view_list.append({
-            'url': apps.get_app_config(mod).admin_url_name,
-            'name': name,
-            })
-
-    return sorted(view_list, key=lambda x: x['name'])
+    if VIEW_LIST == []:
+        for mod in mod_list:
+            if '.' in mod:
+                name = mod.split('.')[-1]
+                mod = mod.split('.')[0]
+            else:
+                name = mod
+            VIEW_LIST.append({
+                'url': apps.get_app_config(mod).admin_url_name,
+                'name': name,
+                })
+        VIEW_LIST = sorted(VIEW_LIST, key=lambda x: x['name'])
+    return VIEW_LIST
