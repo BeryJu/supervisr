cc29a8f5a6ad63cb81c8f018850927c8b457617b
Jens Langhammer <jens.langhammer@beryju.org>
2017-03-25 16:33:55 +0100

    add user settings

---
+++ supervisr/forms/user.py
@@ -0,0 +1,19 @@
+"""
+Supervisr Core User Forms
+"""
+
+
+from django import forms
+from .core import InlineForm
+
+class EditUserForm(InlineForm):
+    """
+    Form to edit a User
+    """
+
+    order = ['name', 'email', 'unix_username', 'unix_userid', 'news_accept']
+    name = forms.CharField(label='Name')
+    email = forms.CharField(label='Email', disabled=True, required=False)
+    unix_username = forms.CharField(label='Unix Username', disabled=True, required=False)
+    unix_userid = forms.CharField(label='Unix ID', disabled=True, required=False)
+    news_accept = forms.BooleanField(required=False, label='Subscribe to Newsletters')
