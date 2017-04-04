d2ef36c69513c7289255e717434b12eb355eed77
Jens Langhammer <jens.langhammer@beryju.org>
2017-04-04 21:52:11 +0200

    close #35 and #36

--- supervisr_mod_2fa/middleware.py
+++ supervisr_mod_2fa/middleware.py
@@ -4,6 +4,7 @@ Supervisr 2FA Middleware to force users with 2FA set up to verify
 
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.http import urlencode
 from django_otp import user_has_device
 
 
@@ -22,7 +23,12 @@ def tfa_force_verify(get_response):
             req.path != reverse('supervisr_mod_2fa:tfa-verify') and \
             not req.META.get('HTTP_AUTHORIZATION', '').startswith('Bearer'):
             # User has 2FA set up but is not verified
-            return redirect(reverse('supervisr_mod_2fa:tfa-verify'))
+
+            # Check if request has a next arguemnt and keep it
+            args = ''
+            if 'next' in req.GET:
+                args = '?' + urlencode({'next': req.GET.get('next')})
+            return redirect(reverse('supervisr_mod_2fa:tfa-verify')+args)
 
         response = get_response(req)
         return response
