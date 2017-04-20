92a3560d512cb6b41983ba3e95b479f4b9487530
Jens Langhammer <jens.langhammer@beryju.org>
2017-04-20 20:49:01 +0200

    first implementation of ifapp decorator

--- supervisr/decorators.py
+++ supervisr/decorators.py
@@ -4,6 +4,7 @@ supervisr view decorators
 
 import time
 
+from django.conf import settings
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.http import urlencode
@@ -56,3 +57,42 @@ def reauth_required(view_function):
     wrap.__doc__ = view_function.__doc__
     wrap.__name__ = view_function.__name__
     return wrap
+
+APP_CACHE = None
+
+def ifapp(app_name):
+    """
+    Only executes ifapp_func if app_name is installed
+    """
+    # pylint: disable=global-statement
+    global APP_CACHE
+    # Make a list of all short names for all apps
+    if not APP_CACHE:
+        APP_CACHE = []
+        for app in settings.INSTALLED_APPS:
+            if '.' in app:
+                parts = app.split('.')
+                if parts[0] == 'supervisr':
+                    APP_CACHE.append(parts[1])
+                else:
+                    APP_CACHE.append(parts[0])
+            else:
+                APP_CACHE.append(app)
+    def outer_wrap(ifapp_func):
+        """
+        Only executes ifapp_func if app_name is installed
+        """
+        def wrap(*args, **kwargs):
+            """
+            Only executes ifapp_func if app_name is installed
+            """
+            if app_name in APP_CACHE:
+                return ifapp_func(*args, **kwargs)
+            else:
+                return
+        wrap.__doc__ = ifapp_func.__doc__
+        wrap.__name__ = ifapp_func.__name__
+
+        return wrap
+
+    return outer_wrap
