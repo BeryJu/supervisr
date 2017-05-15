ff0bc1497b7f5d99be03b60a92b935c0f02ee2a6
Jens Langhammer <jens.langhammer@beryju.org>
2017-05-15 18:42:44 +0200

    [saml-idp] add app structure

---
+++ supervisr/mod/saml-idp/apps.py
@@ -0,0 +1,13 @@
+"""
+Supervisr mod saml_idp app config
+"""
+
+from supervisr.core.apps import SupervisrAppConfig
+
+
+class SupervisrModSAMLIDPConfig(SupervisrAppConfig):
+    """
+    Supervisr mod saml_idp app config
+    """
+
+    name = 'supervisr.mod.saml_idp'
