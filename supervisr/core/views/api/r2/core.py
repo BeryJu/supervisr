13e67d9168f0a2d6b178e923f1fd1e84631d1412
Jens Langhammer <jens.langhammer@beryju.org>
2017-06-08 11:40:06 +0200

    [core/api] add health endpoint to check system health. Progress to #37

---
+++ supervisr/core/views/api/r2/core.py
@@ -0,0 +1,37 @@
+"""
+Supervisr Core APIv2
+"""
+
+from django.core.cache import CacheKeyWarning, cache
+from django.db import connections
+from django.db.utils import OperationalError
+from oauth2_provider.decorators import protected_resource
+
+from supervisr.core.views.api.utils import api_response
+
+
+def _db_status():
+    db_conn = connections['default']
+    try:
+        c = db_conn.cursor()
+    except OperationalError:
+        return False
+    else:
+        return True
+
+def _cache_status():
+    cache.set('djangohealtcheck_test', 'itworks', 1)
+    if not cache.get("djangohealtcheck_test") == "itworks":
+        return False
+    return True
+
+@protected_resource()
+def health(req):
+    """
+    Return oursevles as json
+    """
+    data = {
+        'database': _db_status(),
+        'cache': _cache_status(),
+    }
+    return api_response(req, data)
