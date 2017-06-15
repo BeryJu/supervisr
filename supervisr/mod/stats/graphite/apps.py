a829a7690bfa0cab943370813313b798f5119ccf
Jens Langhammer <jens.langhammer@beryju.org>
2017-06-15 17:09:21 +0200

    [mod_graphite] add thread to send memory and CPU usage every 10s

--- supervisr/mod/stats/graphite/apps.py
+++ supervisr/mod/stats/graphite/apps.py
@@ -2,9 +2,18 @@
 Supervisr Stats Graphite AppConfig
 """
 
+import logging
+import os
+import socket
+import threading
+import time
+
+import psutil
+from django.conf import settings
 
 from supervisr.core.apps import SupervisrAppConfig
 
+LOGGER = logging.getLogger(__name__)
 
 class SupervisrModStatGraphiteConfig(SupervisrAppConfig):
     """
@@ -12,3 +21,29 @@ class SupervisrModStatGraphiteConfig(SupervisrAppConfig):
     """
 
     name = 'supervisr.mod.stats.graphite'
+
+    def ready(self):
+        super(SupervisrModStatGraphiteConfig, self).ready()
+
+        def send_stats(client, host):
+            """
+            Statistics checker function
+            """
+            process = psutil.Process(os.getpid())
+            client.write('%s.mem' % host, process.memory_info().rss / 1024 / 1024)
+            client.write('%s.cpu' % host, process.cpu_percent())
+            time.sleep(10)
+            send_stats(client, host)
+
+        from supervisr.core.models import Setting
+        from supervisr.mod.stats.graphite.graphite_client import GraphiteClient
+
+        if Setting.objects.get(pk='mod:stats:graphite:enabled').value_bool:
+            with GraphiteClient() as client:
+                if settings.DEBUG:
+                    LOGGER.info("Not starting StatsThread because DEBUG")
+                else:
+                    thread = threading.Thread(target=send_stats,
+                                              args=(client, socket.gethostname(), ))
+                    thread.start()
+                    LOGGER.info("Started Statistics sender in seperate Thread")
