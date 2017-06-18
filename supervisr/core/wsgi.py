abf8e74c666ed071bddcbb0102e9879713bd74c7
Jens Langhammer <jens.langhammer@beryju.org>
2017-06-18 18:27:18 +0200

    [core] [graphite] fix imports (again), switch to while True loop

--- supervisr/core/wsgi.py
+++ supervisr/core/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 import pymysql
+from django.core.wsgi import get_wsgi_application
 
 pymysql.install_as_MySQLdb()
 
