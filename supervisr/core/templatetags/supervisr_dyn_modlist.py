0774056f06d21748df7e16f39bc3ad63f3785735
Jens Langhammer <jens.langhammer@beryju.org>
2017-07-01 18:05:03 +0200

    [core] switch to cache for templatetags, closes #72

--- supervisr/core/templatetags/supervisr_dyn_modlist.py
+++ supervisr/core/templatetags/supervisr_dyn_modlist.py
@@ -8,24 +8,20 @@ from django.apps import apps
 from ..utils import get_apps
 
 register = template.Library()
-VIEW_LIST = []
 
 @register.simple_tag(takes_context=True)
 def supervisr_dyn_modlist(context):
     """
     Get a list of all modules and their admin page
     """
-    # pylint: disable=global-statement
-    global VIEW_LIST
     mod_list = get_apps(mod_only=True)
-    if VIEW_LIST == []:
-        for mod in mod_list:
-            mod = mod.split('.')[:-2][-1]
-            config = apps.get_app_config(mod)
-            title = config.title_moddifier(config.label, context.request)
-            VIEW_LIST.append({
-                'url': apps.get_app_config(mod).admin_url_name,
-                'name': title,
-                })
-        VIEW_LIST = sorted(VIEW_LIST, key=lambda x: x['name'])
-    return VIEW_LIST
+    view_list = []
+    for mod in mod_list:
+        mod = mod.split('.')[:-2][-1]
+        config = apps.get_app_config(mod)
+        title = config.title_moddifier(config.label, context.request)
+        view_list.append({
+            'url': apps.get_app_config(mod).admin_url_name,
+            'name': title,
+            })
+    return sorted(view_list, key=lambda x: x['name'])
--- supervisr/core/templatetags/supervisr_dyn_user.py
+++ supervisr/core/templatetags/supervisr_dyn_user.py
@@ -9,31 +9,27 @@ from ..utils import get_apps
 
 register = template.Library()
 
-APP_LIST = []
-
 @register.simple_tag(takes_context=True)
 def supervisr_dyn_user(context):
     """
     Get a list of modules that have custom user settings
     """
     # pylint: disable=global-statement
-    global APP_LIST
+    app_list = []
     sub_apps = get_apps(mod_only=False)
-    if APP_LIST == []:
-        for mod in sub_apps:
-            if 'mod' in mod:
-                mod = mod.split('.')[:-2][-1]
-            else:
-                mod = mod.split('.')[1]
-            config = apps.get_app_config(mod)
-            view = config.view_user_settings
-            if view is not None:
-                view = '%s:%s' % (mod, view)
-                mod = mod.replace('supervisr.', '').replace('mod.', '')
-                title = config.title_moddifier(config.label, context.request)
-                APP_LIST.append({
-                    'title': title,
-                    'view': view
-                    })
-        APP_LIST = sorted(APP_LIST, key=lambda x: x['title'])
-    return APP_LIST
+    for mod in sub_apps:
+        if 'mod' in mod:
+            mod = mod.split('.')[:-2][-1]
+        else:
+            mod = mod.split('.')[1]
+        config = apps.get_app_config(mod)
+        view = config.view_user_settings
+        if view is not None:
+            view = '%s:%s' % (mod, view)
+            mod = mod.replace('supervisr.', '').replace('mod.', '')
+            title = config.title_moddifier(config.label, context.request)
+            app_list.append({
+                'title': title,
+                'view': view
+                })
+    return sorted(app_list, key=lambda x: x['title'])
