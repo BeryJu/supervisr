ecb297e09a61585b98aa7e07ca362215e4202c14
Jens Langhammer <jens.langhammer@beryju.org>
2017-07-04 10:57:42 +0200

    catch more errors, switch back to mysql

--- supervisr/local_settings_mysql.py
+++ supervisr/local_settings_mysql.py
@@ -8,7 +8,7 @@ DATABASES = {
         'NAME': 'supervisr_test',
         'USER': 'root',
         'PASSWORD': 'EK-5jnKfjrGRm<77',
-        'HOST': 'mariadb',
+        'HOST': 'mysql',
         'PORT': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES',character_set_connection=utf8,collation_connection=utf8_unicode_ci"
