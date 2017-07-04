f74a93c94d8b3d690fb3fcd73ed8bbf1822635a0
Jens Langhammer <jens.langhammer@beryju.org>
2017-07-04 08:13:53 +0000

    set chartset upon connection

--- supervisr/local_settings_mysql.py
+++ supervisr/local_settings_mysql.py
@@ -6,12 +6,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'supervisr_test',
-        'USER': 'root',
-        'PASSWORD': 'EK-5jnKfjrGRm<77',
-        'HOST': 'mysql',
+        'USER': 'newuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
         'PORT': '',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES',character_set_connection=utf8,collation_connection=utf8_unicode_ci"
         }
     }
 }
