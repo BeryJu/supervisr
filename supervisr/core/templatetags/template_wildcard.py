50e221280ddc1b73b17c19202c6763e5558affd6
Jens Langhammer <jens.langhammer@beryju.org>
2017-07-05 20:30:56 +0200

    [mod/auth/oauth/client] only show templates if provider exists

--- supervisr/core/templatetags/template_wildcard.py
+++ supervisr/core/templatetags/template_wildcard.py
@@ -11,19 +11,19 @@ from django.conf import settings
 register = template.Library()
 
 @register.simple_tag
-def template_wildcard(tmpl_dir):
+def template_wildcard(*args):
     """
     Return a list of all templates in dir
     """
-    for app in settings.INSTALLED_APPS:
-        if app.startswith('supervisr'):
-            app_dir = '/'.join(app.split('.')[1:-2])
-            prefix = os.path.abspath(os.path.join(settings.BASE_DIR, app_dir, 'templates/'))
-            path = os.path.join(prefix, tmpl_dir)
-            if os.path.isdir(path):
-                files = glob.glob(path+'**')
-                templates = []
-                for file in files:
-                    templates.append(os.path.relpath(file, start=prefix))
-                    return templates
-    return []
+    templates = []
+    for tmpl_dir in args:
+        for app in settings.INSTALLED_APPS:
+            if app.startswith('supervisr'):
+                app_dir = '/'.join(app.split('.')[1:-2])
+                prefix = os.path.abspath(os.path.join(settings.BASE_DIR, app_dir, 'templates/'))
+                path = os.path.join(prefix, tmpl_dir)
+                if os.path.isdir(path):
+                    files = glob.glob(path+'**')
+                    for file in files:
+                        templates.append(os.path.relpath(file, start=prefix))
+    return templates
