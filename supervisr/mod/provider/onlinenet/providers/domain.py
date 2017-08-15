ef87583a39085d01d7919a9376343d56668b5918
Jens Langhammer <jens.langhammer@beryju.org>
2017-08-15 21:02:26 +0200

    fix unittests

--- supervisr/mod/provider/onlinenet/providers/domain.py
+++ supervisr/mod/provider/onlinenet/providers/domain.py
@@ -6,16 +6,32 @@ import logging
 
 from supervisr.core.providers.domain import DomainProvider
 
-from supervisr.mod.provider.onlinenet.providers.core import OnlineNetProvider
-
 LOGGER = logging.getLogger(__name__)
 
 # pylint: disable=too-few-public-methods
-class OnlineNetDomainProvider(OnlineNetProvider, DomainProvider):
+class OnlineNetDomainProvider(DomainProvider):
     """
     OnlineNet provider
     """
     parent = None
+
+    def check_credentials(self, credentials=None):
+        """
+        Check if Credentials is the correct class and try authentication.
+        credentials might be none, in which case credentials from the constructor should be used.
+        Should return False if check fails, otherwise True
+        """
+        return self.parent.check_credentials(credentials)
+
+    def check_status(self):
+        """
+        This is used to check if the provider is reachable
+        Expected Return values:
+         - True: Everything is ok
+         - False: Error (show generic warning)
+         - String: Error (show string)
+        """
+        return self.parent.check_status()
 
     def register(self, domain, **kwargs):
         """
