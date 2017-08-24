121713c551c7184f53617f5fbaeeb5e1d5243c1f
Jens Langhammer <jens.langhammer@beryju.org>
2017-08-24 22:09:08 +0200

    [mod/stats/graphite] only register handler after everything has been loaded

--- supervisr/mod/stats/graphite/apps.py
+++ supervisr/mod/stats/graphite/apps.py
@@ -24,6 +24,36 @@ class SupervisrModStatGraphiteConfig(SupervisrAppConfig):
     label = 'supervisr/mod/stats/graphite'
     title_moddifier = lambda self, title, request: 'Stats/Graphite'
 
+    def __init__(self, *args, **kwargs):
+        super(SupervisrModStatGraphiteConfig, self).__init__(*args, **kwargs)
+        @receiver(SIG_GET_SCHEDULER)
+        # pylint: disable=unused-argument
+        def handle_stats_graphite(sender, scheduler, **kwargs):
+            """
+            Register Schedule handler
+            """
+            from supervisr.core.models import Setting
+            from supervisr.mod.stats.graphite.graphite_client import GraphiteClient
+
+            client = GraphiteClient()
+
+            def send_stats(client, host):
+                """
+                Statistics checker function
+                """
+                def send():
+                    """
+                    Send CPU and Memory usage
+                    """
+                    process = psutil.Process(os.getpid())
+                    client.write('server.%s.mem' % host, process.memory_info().rss / 1024 / 1024)
+                    client.write('server.%s.cpu' % host, process.cpu_percent())
+                return send
+
+            if Setting.get('enabled', default='False') != 'False':
+                client.connect()
+                scheduler.every(10).seconds.do(send_stats(client, socket.gethostname()))
+
     def ensure_settings(self):
         return {
             'enabled': False,
@@ -31,31 +61,3 @@ class SupervisrModStatGraphiteConfig(SupervisrAppConfig):
             'port': 2003,
             'prefix': 'supervisr',
         }
-
-@receiver(SIG_GET_SCHEDULER)
-# pylint: disable=unused-argument
-def handle_stats_graphite(sender, scheduler, **kwargs):
-    """
-    Register Schedule handler
-    """
-    from supervisr.core.models import Setting
-    from supervisr.mod.stats.graphite.graphite_client import GraphiteClient
-
-    client = GraphiteClient()
-
-    def send_stats(client, host):
-        """
-        Statistics checker function
-        """
-        def send():
-            """
-            Send CPU and Memory usage
-            """
-            process = psutil.Process(os.getpid())
-            client.write('server.%s.mem' % host, process.memory_info().rss / 1024 / 1024)
-            client.write('server.%s.cpu' % host, process.cpu_percent())
-        return send
-
-    if Setting.get('enabled', default='False') != 'False':
-        client.connect()
-        scheduler.every(10).seconds.do(send_stats(client, socket.gethostname()))
