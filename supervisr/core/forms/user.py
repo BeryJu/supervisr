ebb758612ccb0746e9f4d7c9f40bdd04d41095b0
Jens Langhammer <jens.langhammer@beryju.org>
2017-08-24 20:24:30 +0200

    [core] add feedback view
    closes #200

--- supervisr/core/forms/user.py
+++ supervisr/core/forms/user.py
@@ -4,8 +4,9 @@ Supervisr Core User Forms
 
 
 from django import forms
+from django.utils.translation import ugettext as _
 
-from .core import InlineForm
+from supervisr.core.forms.core import InlineForm
 
 
 class EditUserForm(InlineForm):
@@ -14,8 +15,18 @@ class EditUserForm(InlineForm):
     """
 
     order = ['name', 'email', 'username', 'unix_username', 'unix_userid']
-    name = forms.CharField(label='Name')
-    email = forms.CharField(label='Email', disabled=True, required=False)
-    username = forms.CharField(label='Username')
-    unix_username = forms.CharField(label='Unix Username', disabled=True, required=False)
-    unix_userid = forms.CharField(label='Unix ID', disabled=True, required=False)
+    name = forms.CharField(label=_('Name'))
+    email = forms.CharField(label=_('Email'), disabled=True, required=False)
+    username = forms.CharField(label=_('Username'))
+    unix_username = forms.CharField(label=_('Unix Username'), disabled=True, required=False)
+    unix_userid = forms.CharField(label=_('Unix ID'), disabled=True, required=False)
+
+class FeedbackForm(InlineForm):
+    """
+    Form to send feedback
+    """
+
+    order = ['email', 'message', 'send_system_info']
+    email = forms.CharField(label=_('Email'), disabled=True, required=False)
+    message = forms.CharField(widget=forms.Textarea, label=_('Message'))
+    send_system_info = forms.BooleanField(label=_('Send System Information?'), required=False)
--- supervisr/core/views/user.py
+++ supervisr/core/views/user.py
@@ -6,9 +6,11 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 
-from ..forms.user import EditUserForm
-from ..models import Event
+from supervisr.core.forms.user import EditUserForm, FeedbackForm
+from supervisr.core.mailer import send_message
+from supervisr.core.models import Event
 
 
 @login_required
@@ -30,7 +32,7 @@ def index(req):
             req.user.save()
             req.user.userprofile.username = form.cleaned_data.get('username')
             req.user.userprofile.save()
-            messages.success(req, 'User updated successfully')
+            messages.success(req, _('User updated successfully'))
     else:
         form = EditUserForm(initial=initial_data)
     return render(req, 'user/index.html', {'form': form})
@@ -53,3 +55,24 @@ def events(req):
         event_page = paginator.page(paginator.num_pages)
 
     return render(req, 'user/events.html', {'events': event_page})
+
+@login_required
+def send_feedback(req):
+    """
+    Show Form to send feedback
+    """
+    if req.method == 'POST':
+        form = FeedbackForm(req.POST, initial={'email': req.user.email})
+        if form.is_valid():
+            email = req.user.email
+            text = form.cleaned_data.get('text')
+            send_message(['jens@beryju.org'], '[Supervisr] Feedback from %s' % email,
+                         text='User %s sent feedback: %s' % (email, text))
+            messages.success(req, _('Successfully sent feedback.'))
+
+    form = FeedbackForm(initial={'email': req.user.email})
+    return render(req, 'user/feedback.html', {
+        'title': 'Send Feedback',
+        'primary_action': 'Send',
+        'form': form
+        })
