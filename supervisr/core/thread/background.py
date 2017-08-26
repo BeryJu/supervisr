8125001bda4867d8a00650e3f3dd4a595021e128
Jens Langhammer <jens.langhammer@beryju.org>
2017-08-26 20:22:13 +0200

    [core] change background thread to not use a signal
    [mod/stats/graphite] fix code running to early

--- supervisr/core/thread/background.py
+++ supervisr/core/thread/background.py
@@ -5,10 +5,10 @@ import logging
 import time
 from threading import Thread
 
-from django.dispatch import Signal
 from schedule import Scheduler
 
 LOGGER = logging.getLogger(__name__)
+SCHEDULER = Scheduler()
 
 
 class BackgroundThread(Thread):
@@ -18,17 +18,13 @@ class BackgroundThread(Thread):
 
     def __init__(self):
         super(BackgroundThread, self).__init__()
-        self.schedule = Scheduler()
         self.daemon = True
         self.name = 'Supervisr Background Thread'
-        SIG_GET_SCHEDULER.send(sender=self, scheduler=self.schedule)
 
     def run(self):
         """
         Run Scheduler
         """
         while True:
-            self.schedule.run_pending()
+            SCHEDULER.run_pending()
             time.sleep(1)
-
-SIG_GET_SCHEDULER = Signal(providing_args=['scheduler'])
