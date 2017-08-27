1aa4269d297f6c29738933356397ede3d634c272
Jens Langhammer <jens.langhammer@beryju.org>
2017-08-27 19:11:46 +0200

    [core] rework tasks

---
+++ supervisr/_tasks/supervisr.py
@@ -0,0 +1,89 @@
+"""
+Supervisr Invoke Tasks
+"""
+import logging
+
+from invoke import task
+
+try:
+    import django
+    from django.db.utils import IntegrityError
+except ImportError:
+    print("Django could not be imported")
+
+LOGGER = logging.getLogger(__name__)
+
+@task()
+# pylint: disable=unused-argument
+def migrate(ctx):
+    """
+    Apply migrations
+    """
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'migrate'])
+
+@task()
+# pylint: disable=unused-argument
+def list_users(ctx):
+    """
+    Show a list of all users
+    """
+    django.setup()
+    from django.contrib.auth.models import User
+    users = User.objects.all().order_by('pk')
+    LOGGER.info("Listing users...")
+    for user in users:
+        LOGGER.info("id=%d username=%s", user.pk, user.username)
+
+@task
+# pylint: disable=unused-argument
+def make_superuser(ctx, uid):
+    """
+    Make User with uid superuser.
+    This should be run after `./manage.py createsuperuser` or
+    after you sign up on the webinterface as first user
+    """
+    django.setup()
+    from supervisr.core.models import UserProfile
+    from django.contrib.auth.models import User
+    user = User.objects.filter(pk=uid).first()
+    LOGGER.info("About to make '%s' superuser...", user.username)
+    user.is_staff = True
+    user.is_superuser = True
+    user.is_active = True
+    user.save()
+    try:
+        # UserProfile might already exist
+        # if user signed up with webinterface
+        UserProfile.objects.create(user=user)
+    except IntegrityError:
+        pass
+    LOGGER.info("Done!")
+
+@task
+# pylint: disable=unused-argument
+def run(ctx, pidfile=''):
+    """
+    Run CherryPY-based application server
+    """
+    from supervisr.core.wsgi import application
+    from cherrypy.process.plugins import PIDFile
+    import cherrypy
+    cherrypy.config.update({'log.screen': False,
+                            'log.access_file': '',
+                            'log.error_file': ''})
+    cherrypy.tree.graft(application, "/")
+    cherrypy.server.unsubscribe()
+    # pylint: disable=protected-access
+    server = cherrypy._cpserver.Server()
+
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 8080
+    server.thread_pool = 30
+    server.subscribe()
+
+    if pidfile != '':
+        PIDFile(cherrypy.engine, pidfile).subscribe()
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
