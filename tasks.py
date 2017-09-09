e52320a9dcf3b91ebe6c9b28c728ac821e9fcb91
Jens Langhammer <jens.langhammer@beryju.org>
2017-09-09 14:25:01 +0200

    [tasks] fix tasks not working

--- tasks.py
+++ tasks.py
@@ -3,10 +3,15 @@ Supervisr Invoke Tasks
 """
 import os
 
-import pymysql
 from invoke import Collection
 
 from supervisr._tasks import dev, supervisr
+
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supervisr.core.settings")
 os.environ.setdefault("SUPERVISR_LOCAL_SETTINGS", "supervisr.local_settings")
