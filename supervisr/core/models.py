96177aa31c1fb555be27c5b2c93dca3d81efaa40
Jens Langhammer <jens.langhammer@beryju.org>
2017-11-05 17:11:51 +0100

    [core] fix get_random_string sometimes being shorter than expected

--- supervisr/core/models.py
+++ supervisr/core/models.py
@@ -53,8 +53,8 @@ def get_random_string(length=10):
     """
     # Generate a normal UUID, convert it to base64 and take the 10 first chars
     uid = uuid.uuid4()
-    # UUID in base64 is 25 chars, we want 10 char length
-    offset = random.randint(0, 25-length)
+    # UUID in base64 is 25 chars, we want *length* char length
+    offset = random.randint(0, 25 - length - 1)
     # Python3 changed the way we need to encode
     res = base64.b64encode(uid.bytes, altchars=b'_-')
     return res[offset:offset+length].decode("utf-8")
