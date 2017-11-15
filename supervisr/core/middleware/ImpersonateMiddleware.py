bb00ceb65de83e2c0dc54ec080c6cbbd0e3e467b
Jens Langhammer <jens.langhammer@beryju.org>
2017-11-15 18:05:36 +0100

    [core] show impersonation as red username, dont show permanent message anymore

--- supervisr/core/middleware/ImpersonateMiddleware.py
+++ supervisr/core/middleware/ImpersonateMiddleware.py
@@ -1,21 +1,10 @@
-"""
-Supervisr Core Middleware to impersonate users
-"""
+"""Supervisr Core Middleware to impersonate users"""
 
-from django.contrib import messages
 from django.contrib.auth.models import User
-from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
-
-from supervisr.core.utils import messages_add_once
 
 
 def impersonate(get_response):
     """Middleware to impersonate users"""
-
-    stop_text = _('Stop Impersonating')
-    stop_link = mark_safe('<a class="btn alert-action" href="?__unimpersonate=True">%s</a>' \
-        % stop_text)
 
     def middleware(req):
         """Middleware to impersonate users"""
@@ -26,8 +15,7 @@ def impersonate(get_response):
             del req.session['impersonate_id']
         if req.user.is_superuser and 'impersonate_id' in req.session:
             req.user = User.objects.get(id=req.session['impersonate_id'])
-            messages_add_once(req, messages.ERROR, _("You are impersonating %s. %s"
-                                                     % (req.user.email, stop_link)))
+
         response = get_response(req)
         return response
     return middleware
