81ef39699e80c5b3834704f96afdd4ba4305dbf1
Jens Langhammer <jens.langhammer@beryju.org>
2017-11-30 20:02:17 +0100

    [core] improve API status codes, return 403 when not authenticated

--- supervisr/core/api/utils.py
+++ supervisr/core/api/utils.py
@@ -19,12 +19,13 @@ def api_response(req, data):
 
     _globals = globals()
     handler_name = 'api_response_%s' % selected_format
+    code = data.get('code', 200)
     handler = _globals[handler_name] if handler_name in _globals else None
     if handler is not None:
-        return handler(data)
+        return handler(data=data, code=code)
     return JsonResponse({'error': "type '%s' not found" % selected_format})
 
-def api_response_openid(data):
+def api_response_openid(code, data):
     """
     This method only renames some keys for OpenID, then uses JSON
     """
@@ -38,20 +39,20 @@ def api_response_openid(data):
         if init_key in data:
             data[dest_key] = data[init_key]
             del data[init_key]
-    return api_response_json(data)
+    return api_response_json(code, data)
 
-def api_response_json(data):
+def api_response_json(code, data):
     """
     Pass dict to django and let it handle the encoding etc
     """
     import json
     data = json.dumps(data, sort_keys=True, indent=4)
-    return HttpResponse(data, content_type='application/json')
+    return HttpResponse(data, content_type='application/json', status=code)
 
-def api_response_yaml(data):
+def api_response_yaml(code, data):
     """
     Return data as yaml dict
     """
     import yaml
     return HttpResponse(yaml.dump(data, default_flow_style=False),
-                        content_type='text/x-yaml')
+                        content_type='text/x-yaml', status=code)
