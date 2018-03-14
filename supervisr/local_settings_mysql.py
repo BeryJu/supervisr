9a56dbd7853523779ad3857facc9880d18951c5d
Jens Langhammer <jens.langhammer@beryju.org>
2018-03-14 21:28:07 +0100

    fix mysql config

--- supervisr/local_settings_mysql.py
+++ supervisr/local_settings_mysql.py
@@ -1,7 +1,5 @@
 # pylint: skip-file
-"""
-MySQL Local settings
-"""
+"""MySQL Local settings"""
 
 DATABASES = {
     'default': {
@@ -12,12 +10,12 @@ DATABASES = {
         'HOST': 'mysql',
         'PORT': '',
         'OPTIONS': {
-            'init_command': """
-            SET sql_mode='STRICT_TRANS_TABLES';
-            SET character_set_connection='utf8';
-            SET collation_connection='utf8_unicode_ci';
-            ALTER DATABASE supervisr_test CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-            """
+            'init_command': (
+                "SET sql_mode='STRICT_TRANS_TABLES';",
+                "SET character_set_connection='utf8';",
+                "SET collation_connection='utf8_unicode_ci';",
+                "ALTER DATABASE supervisr_test CHARACTER SET utf8 COLLATE utf8_unicode_ci;",
+            )
         }
     }
 }
