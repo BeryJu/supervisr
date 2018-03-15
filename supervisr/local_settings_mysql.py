ff874fe5236550508ce40aaeb52d622e262702bb
Jens Langhammer <jens.langhammer@beryju.org>
2018-03-15 10:22:46 +0100

    [ci] fix mysql settings again

--- supervisr/local_settings_mysql.py
+++ supervisr/local_settings_mysql.py
@@ -10,12 +10,10 @@ DATABASES = {
         'HOST': 'mysql',
         'PORT': '',
         'OPTIONS': {
-            'init_command': (
-                "SET sql_mode='STRICT_TRANS_TABLES';"
-                "SET character_set_connection='utf8';"
-                "SET collation_connection='utf8_unicode_ci';"
-                "ALTER DATABASE supervisr_test CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-            )
+            'charset': 'utf8',
+            'sql_mode': 'STRICT_TRANS_TABLES',
+            'init_command': "ALTER DATABASE supervisr_test "
+                            "CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
         }
     }
 }
