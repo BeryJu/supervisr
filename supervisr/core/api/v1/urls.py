66d639f3990f8f1964dd38806562a600f7c5fed5
Jens Langhammer <jens.langhammer@beryju.org>
2018-07-27 12:53:37 +0200

    [core] add http_basic_auth API endpoint

--- supervisr/core/api/v1/urls.py
+++ supervisr/core/api/v1/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf.urls import url
 
-from supervisr.core.api.v1.accounts import AccountAPI
+from supervisr.core.api.v1.accounts import AccountAPI, http_basic_auth
 from supervisr.core.api.v1.domains import DomainAPI
 from supervisr.core.api.v1.events import EventAPI
 from supervisr.core.api.v1.providers import ProviderAPI
@@ -10,6 +10,7 @@ from supervisr.core.api.v1.system import SystemAPI
 from supervisr.core.api.v1.tasks import TaskAPI
 
 urlpatterns = [
+    url(r'^accounts/http_basic_auth/$', http_basic_auth, name='accounts-http-basic-auth'),
     url(r'^accounts/(?P<verb>\w+)/$', AccountAPI.as_view(), name='accounts'),
     url(r'^domains/(?P<verb>\w+)/$', DomainAPI.as_view(), name='domains'),
     url(r'^events/(?P<verb>\w+)/$', EventAPI.as_view(), name='events'),
--- supervisr/core/decorators.py
+++ supervisr/core/decorators.py
@@ -25,13 +25,11 @@ REAUTH_MARGIN = getattr(settings, 'REAUTH_MARGIN', 300)
 
 
 def anonymous_required(view_function):
-    """
-    Decorator to only allow a view for anonymous users
-    """
+    """Decorator to only allow a view for anonymous users"""
+
     def wrap(*args, **kwargs):
-        """
-        Check if request's user is authenticated and route back to index
-        """
+        """Check if request's user is authenticated and route back to index"""
+
         req = args[0] if args else None
         if req and req.user is not None and req.user.is_authenticated:
             return redirect(reverse('common-index'))
