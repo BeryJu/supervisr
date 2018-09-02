627b05338f8d366d2cd8cad0d74467dae8bb61be
Jens Langhammer <jens.langhammer@beryju.org>
2018-09-02 23:52:03 +0200

    [core] fix requirements

--- supervisr/core/__init__.py
+++ supervisr/core/__init__.py
@@ -9,4 +9,4 @@ __version__ = '0.1.2.2'
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 
-__all__ = ('celery_app',)
+# __all__ = ('celery_app',)
