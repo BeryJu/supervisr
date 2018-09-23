86d91445c302c223567a2d4ee0467458cf748635
Jens Langhammer <jens.langhammer@beryju.org>
2018-09-23 08:46:23 +0200

    [core] implement bumpversion to automate versioning, start with 0.2.0-alpha

--- supervisr/core/__init__.py
+++ supervisr/core/__init__.py
@@ -1,12 +1,6 @@
 """Supervisr Core Header"""
-from __future__ import absolute_import, unicode_literals
 from supervisr.core.celery import CELERY_APP as celery_app # noqa
 __ui_name__ = 'Supervisr Core'
 __author__ = 'Supervisr Team'
 __email__ = 'supervisr@beryju.org'
-__version__ = '0.1.2.2'
-
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-
-# __all__ = ('celery_app',)
+__version__ = '0.2.0-alpha'
