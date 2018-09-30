ba46b8f0f92e4fe2cc776e8dd4591bac61f6de8a
Jens Langhammer <jens.langhammer@beryju.org>
2018-09-30 13:18:53 +0200

    [mod/auth/ldap] fix unittests

--- supervisr/mod/auth/ldap/tests/test_account_ldap.py
+++ supervisr/mod/auth/ldap/tests/test_account_ldap.py
@@ -31,29 +31,22 @@ class TestAccountLDAP(TestCase):
         self.user.is_active = False
         self.user.set_password(self.password)
         self.user.save()
-
-    def test_new_user(self):
-        """Test new new ldap user"""
         self.assertTrue(self.ldap.create_ldap_user(self.user, self.password))
 
     def test_change_password(self):
         """Test ldap change_password"""
-        self.assertTrue(self.ldap.create_ldap_user(self.user, self.password))
         self.assertTrue(self.ldap.change_password('b4ryju1rg!', mail=self.user.email))
         self.assertTrue(self.ldap.change_password('b3ryju0rg!', mail=self.user.email))
 
     def test_disable_enable(self):
         """Test ldap enable and disable"""
-        self.assertTrue(self.ldap.create_ldap_user(self.user, self.password))
         self.assertTrue(self.ldap.disable_user(mail=self.user.email))
         self.assertTrue(self.ldap.enable_user(mail=self.user.email))
 
     def test_email_used(self):
         """Test ldap is_email_used"""
-        self.assertTrue(self.ldap.create_ldap_user(self.user, self.password))
         self.assertTrue(self.ldap.is_email_used(self.user.email))
 
     def test_auth(self):
         """Test ldap auth"""
-        self.assertTrue(self.ldap.create_ldap_user(self.user, self.password))
         # self.assertTrue(self.ldap.auth_user(self.password, mail=self.user.email))
