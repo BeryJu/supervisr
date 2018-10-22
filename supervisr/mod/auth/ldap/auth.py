4d9a836f6c2bcc5aa525846d1f3b42821d9c47aa
Jens Langhammer <jens.langhammer@beryju.org>
2018-10-22 16:17:36 +0200

    [mod/auth/ldap] only load LDAPConnector if LDAP is enabled

--- supervisr/mod/auth/ldap/auth.py
+++ supervisr/mod/auth/ldap/auth.py
@@ -11,16 +11,11 @@ LOGGER = getLogger(__name__)
 class LDAPBackend(ModelBackend):
     """Authenticate users against LDAP Server"""
 
-    _ldap = None
-
-    def __init__(self, *args, **kwargs):
-        super(LDAPBackend, self).__init__(*args, **kwargs)
-        self._ldap = LDAPConnector()
-
     def authenticate(self, **kwargs):
         """Try to authenticate a user via ldap"""
         if 'password' not in kwargs:
             return None
         if not LDAPConnector.enabled:
             return None
+        _ldap = LDAPConnector()
         return self._ldap.auth_user(**kwargs)
