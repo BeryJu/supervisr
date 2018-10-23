89807debc898641fccf50f54540c7c6879221458
Jens Langhammer <jens.langhammer@beryju.org>
2018-10-23 22:32:26 +0200

    [dns] add missing ReverseZone API

--- supervisr/dns/api/v1/urls.py
+++ supervisr/dns/api/v1/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import url
 
 from supervisr.core.utils.constants import UUID_REGEX
 from supervisr.dns.api.v1.record import RecordAPI, dyndns_update
-from supervisr.dns.api.v1.zone import ZoneAPI
+from supervisr.dns.api.v1.zone import ReverseZoneAPI, ZoneAPI
 
 urlpatterns = [
     url(r'^records/(?P<verb>\w+)/$', RecordAPI.as_view(), name='api-v1-records'),
     url(r'^records/(?P<record_uuid>%s)/dyndns/$' % UUID_REGEX,
         dyndns_update, name='api-v1-zone-dyndns'),
     url(r'^zones/(?P<verb>\w+)/$', ZoneAPI.as_view(), name='api-v1-zone'),
+    url(r'^zones/reverse/(?P<verb>\w+)/$', ReverseZoneAPI.as_view(), name='api-v1-reverse-zone'),
 ]
--- supervisr/dns/api/v1/zone.py
+++ supervisr/dns/api/v1/zone.py
@@ -1,8 +1,9 @@
 """Supervisr DNS v1 Zone API"""
 
 from supervisr.core.api.models import UserAcquirableModelAPI
+from supervisr.dns.forms.reverse_zones import ReverseZoneForm
 from supervisr.dns.forms.zones import ZoneForm
-from supervisr.dns.models import Zone
+from supervisr.dns.models import ReverseZone, Zone
 
 
 class ZoneAPI(UserAcquirableModelAPI):
@@ -10,3 +11,10 @@ class ZoneAPI(UserAcquirableModelAPI):
 
     model = Zone
     form = ZoneForm
+
+
+class ReverseZoneAPI(UserAcquirableModelAPI):
+    """ReverseZone API"""
+
+    model = ReverseZone
+    form = ReverseZoneForm
