64268cade924a8ba8032bbd32bf1d8d104640419
Jens Langhammer <jens.langhammer@beryju.org>
2018-11-03 21:19:55 +0100

    [mod/provider/nix_dns] fix missing __init__ from bumpversion

--- supervisr/mod/provider/nix_dns/__init__.py
+++ supervisr/mod/provider/nix_dns/__init__.py
@@ -2,5 +2,5 @@
 __ui_name__ = 'Supervisr mod/mod/provider/nix_dns'
 __author__ = 'Supervisr Team'
 __email__ = 'supervisr@beryju.org'
-__version__ = '0.3.5-alpha'
+__version__ = '0.3.14-alpha'
 default_app_config = 'supervisr.mod.provider.nix_dns.apps.SupervisrModProviderNixDNSConfig'
